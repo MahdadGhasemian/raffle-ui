e511ce0a900c203c6fd89bc05c8db635adc132c6
Mahdad Ghasemian <mahdad.ghasemian@gmail.com>
2023-09-11 17:19:35 +0330

    suport sepolia name on metamask

--- src/components/Raffle/ContractInfoProvider.tsx
+++ src/components/Raffle/ContractInfoProvider.tsx
@@ -25,18 +25,12 @@ import ABI from '@/constants/abi.json'
 const ContractInfoProvider: FC<ContractInfoProviderProps> = ({ children }) => {
   const contractAddresses = ContractAddresses as Array<ContractAddressType>
 
-  console.log({ contractAddresses })
-  console.log({ ABI })
-
   const { chain } = useNetwork()
   const networkName = chain?.name.toLowerCase()
   const contractInfo = contractAddresses.find(
     data => data.networkName === networkName || data.networkName2 === networkName
   )
   const contractAddress = contractInfo?.contractAddress
-
-  console.log(chain)
-  console.log({ networkName })
 
   if (!contractAddress) return <Typography>آدرس قرارداد هوشمند پیدا نشد!</Typography>
 
